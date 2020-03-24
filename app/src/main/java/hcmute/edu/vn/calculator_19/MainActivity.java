01e172ae71084584674d0673161fe342f9821145
minhtan143 <minhtan143@users.noreply.github.com>
2020-03-24 20:56:56 +0700

    layout_edit

--- app/src/main/java/hcmute/edu/vn/calculator_19/MainActivity.java
+++ app/src/main/java/hcmute/edu/vn/calculator_19/MainActivity.java
@@ -3,12 +3,36 @@ package hcmute.edu.vn.calculator_19;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtScreen;
+    Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnAdd, btnSubtract, btnMultiply, btnDivide, btnDot, btnDelete, btnEqual;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtScreen = (TextView) findViewById(R.id.txtScreen);
+        btn0 = (Button) findViewById(R.id.btn0);
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+        btn4 = (Button) findViewById(R.id.btn4);
+        btn5 = (Button) findViewById(R.id.btn5);
+        btn6 = (Button) findViewById(R.id.btn6);
+        btn7 = (Button) findViewById(R.id.btn7);
+        btn8 = (Button) findViewById(R.id.btn8);
+        btn9 = (Button) findViewById(R.id.btn9);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnSubtract = (Button) findViewById(R.id.btnSubtract);
+        btnMultiply = (Button) findViewById(R.id.btnMultiply);
+        btnDivide = (Button) findViewById(R.id.btnDivide);
+        btnDot = (Button) findViewById(R.id.btnDot);
+        btnDelete = (Button) findViewById(R.id.btnDelete);
+        btnEqual = (Button) findViewById(R.id.btnEqual);
     }
 }
