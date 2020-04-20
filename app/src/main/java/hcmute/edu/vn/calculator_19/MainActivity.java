41bf446ac29792233c6c63fc67f7881e0493b316
minhtan143 <minhtan143@users.noreply.github.com>
2020-04-20 12:21:25 +0700

    final

--- app/src/main/java/hcmute/edu/vn/calculator_19/MainActivity.java
+++ app/src/main/java/hcmute/edu/vn/calculator_19/MainActivity.java
@@ -8,11 +8,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView txtScreen;
-    Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9,
-            btnAdd, btnSubtract, btnMultiply, btnDivide, btnDot, btnDelete, btnEqual;
     String output, presentNum, tempOperation;
     Boolean isOperation, isNumber, isFirstCalculation, isEqual;
     Double result;
@@ -24,67 +25,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         initParameter();
         restoreStateWhenRotate(savedInstanceState);
-
     }
-
-
-
 
     @Override
     public void onClick(View v) {
         try {
             switch (v.getId()){
-                case R.id.btn0:
-                    clickNumber("0");
-                    break;
-                case R.id.btn1:
-                    clickNumber("1");
-                    break;
-                case R.id.btn2:
-                    clickNumber("2");
-                    break;
-                case R.id.btn3:
-                    clickNumber("3");
-                    break;
-                case R.id.btn4:
-                    clickNumber("4");
-                    break;
-                case R.id.btn5:
-                    clickNumber("5");
-                    break;
-                case R.id.btn6:
-                    clickNumber("6");
-                    break;
-                case R.id.btn7:
-                    clickNumber("7");
-                    break;
-                case R.id.btn8:
-                    clickNumber("8");
-                    break;
-                case R.id.btn9:
-                    clickNumber("9");
-                    break;
-                case R.id.btnDot:
-                    clickDot();
-                    break;
-                case R.id.btnAdd:
-                    clickOperation("+");
-                    break;
-                case R.id.btnSubtract:
-                    clickOperation("-");
-                    break;
-                case R.id.btnDivide:
-                    clickOperation("/");
-                    break;
-                case R.id.btnMultiply:
-                    clickOperation("*");
-                    break;
-                case R.id.btnEqual:
-                    clickEqual();
-                    break;
-                case R.id.btnDelete:
-                    reset();
-                    break;
+                case R.id.btn0: clickNumber("0"); break;
+                case R.id.btn1: clickNumber("1"); break;
+                case R.id.btn2: clickNumber("2"); break;
+                case R.id.btn3: clickNumber("3"); break;
+                case R.id.btn4: clickNumber("4"); break;
+                case R.id.btn5: clickNumber("5"); break;
+                case R.id.btn6: clickNumber("6"); break;
+                case R.id.btn7: clickNumber("7"); break;
+                case R.id.btn8: clickNumber("8"); break;
+                case R.id.btn9: clickNumber("9"); break;
+                case R.id.btnDot: clickDot(); break;
+                case R.id.btnAdd: clickOperation("+"); break;
+                case R.id.btnSubtract: clickOperation("-"); break;
+                case R.id.btnDivide: clickOperation("/"); break;
+                case R.id.btnMultiply: clickOperation("*"); break;
+                case R.id.btnEqual: clickEqual(); break;
+                case R.id.btnDelete: reset(); break;
             }
         }
         catch (Exception ex) {
@@ -96,7 +59,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putDouble("result",result);
+        outState.putDouble("result", result);
         outState.putString("output", output);
         outState.putString("presentNum", presentNum);
         outState.putString("tempOperation", tempOperation);
@@ -106,8 +69,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         outState.putBoolean("isNumber", isNumber);
     }
 
-    public void restoreStateWhenRotate(Bundle savedInstanceState){
-        if (savedInstanceState != null){
+    public void restoreStateWhenRotate(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
             result = savedInstanceState.getDouble("result");
             output = savedInstanceState.getString("output");
             presentNum = savedInstanceState.getString("presentNum");
@@ -119,12 +82,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             if (isNumber)
                 txtScreen.setText(presentNum);
-            else
-                processResult();
+            else processResult();
         }
     }
 
-    public void initParameter(){
+    public void initParameter() {
         result = 0d;
         isOperation = false;
         isNumber = false;
@@ -134,49 +96,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         output = "0";
         presentNum = "";
         txtScreen = findViewById(R.id.txtScreen);
-        btn0 = findViewById(R.id.btn0);
-        btn1 = findViewById(R.id.btn1);
-        btn2 = findViewById(R.id.btn2);
-        btn3 = findViewById(R.id.btn3);
-        btn4 = findViewById(R.id.btn4);
-        btn5 = findViewById(R.id.btn5);
-        btn6 = findViewById(R.id.btn6);
-        btn7 = findViewById(R.id.btn7);
-        btn8 = findViewById(R.id.btn8);
-        btn9 = findViewById(R.id.btn9);
-        btnAdd = findViewById(R.id.btnAdd);
-        btnSubtract = findViewById(R.id.btnSubtract);
-        btnMultiply = findViewById(R.id.btnMultiply);
-        btnDivide = findViewById(R.id.btnDivide);
-        btnDot = findViewById(R.id.btnDot);
-        btnDelete = findViewById(R.id.btnDelete);
-        btnEqual = findViewById(R.id.btnEqual);
 
-        btn0.setOnClickListener(this);
-        btn1.setOnClickListener(this);
-        btn2.setOnClickListener(this);
-        btn3.setOnClickListener(this);
-        btn4.setOnClickListener(this);
-        btn5.setOnClickListener(this);
-        btn6.setOnClickListener(this);
-        btn7.setOnClickListener(this);
-        btn8.setOnClickListener(this);
-        btn9.setOnClickListener(this);
-        btnAdd.setOnClickListener(this);
-        btnSubtract.setOnClickListener(this);
-        btnMultiply.setOnClickListener(this);
-        btnDivide.setOnClickListener(this);
-        btnDot.setOnClickListener(this);
-        btnDelete.setOnClickListener(this);
-        btnEqual.setOnClickListener(this);
+        findViewById(R.id.btn0).setOnClickListener(this);
+        findViewById(R.id.btn1).setOnClickListener(this);
+        findViewById(R.id.btn2).setOnClickListener(this);
+        findViewById(R.id.btn3).setOnClickListener(this);
+        findViewById(R.id.btn4).setOnClickListener(this);
+        findViewById(R.id.btn5).setOnClickListener(this);
+        findViewById(R.id.btn6).setOnClickListener(this);
+        findViewById(R.id.btn7).setOnClickListener(this);
+        findViewById(R.id.btn8).setOnClickListener(this);
+        findViewById(R.id.btn9).setOnClickListener(this);
+        findViewById(R.id.btnAdd).setOnClickListener(this);
+        findViewById(R.id.btnSubtract).setOnClickListener(this);
+        findViewById(R.id.btnMultiply).setOnClickListener(this);
+        findViewById(R.id.btnDivide).setOnClickListener(this);
+        findViewById(R.id.btnDot).setOnClickListener(this);
+        findViewById(R.id.btnDelete).setOnClickListener(this);
+        findViewById(R.id.btnEqual).setOnClickListener(this);
     }
 
-    public void clickNumber(String a){
-        if (isOperation){
+    public void clickNumber(String a) {
+        if (isOperation) {
             output += tempOperation + a;
             presentNum = a;
         }
-        else if (isEqual){
+        else if (isEqual) {
             isFirstCalculation = true;
             isEqual = false;
             output = a;
@@ -200,37 +145,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         isNumber = false;
     }
 
-    public void clickEqual(){
+    public void clickEqual() {
         getResult();
         isEqual = true;
         isNumber = false;
     }
 
-    public void clickDot(){
-
-        if (isOperation){
+    public void clickDot() {
+        if (isOperation) {
             output += tempOperation + ".";
             presentNum = ".";
             isOperation = false;
         }
-        else if (isEqual){
+        else if (isEqual) {
             isFirstCalculation = true;
             isEqual = false;
             output = ".";
             presentNum = ".";
         }
-        else {
-            if (!presentNum.contains(".")){
-                output += ".";
-                presentNum += ".";
-            }
+        else if (!presentNum.contains(".")) {
+            output += ".";
+            presentNum += ".";
         }
         isNumber = true;
         txtScreen.setText(presentNum);
     }
 
-    public void getResult(){
+    public void getResult() {
         result = 0d;
+        BigDecimal temp;
 
         output.trim();
         String[] elementMath = output.split(" ");
@@ -240,46 +183,51 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             isEqual = false;
         }
         else {
-            Double num1 = Double.parseDouble(elementMath[0]);
-            Double num2 = Double.parseDouble(elementMath[2]);
+            BigDecimal num1 = new BigDecimal(elementMath[0]);
+            BigDecimal num2 = new BigDecimal(elementMath[2]);
             switch (elementMath[1]) {
                 case "+":
-                    result = num1 + num2;
+                    temp = num1.add(num2);
+                    result = temp.doubleValue();
                     break;
                 case "-":
-                    result = num1 - num2;
+                    temp = num1.subtract(num2);
+                    result = temp.doubleValue();
                     break;
                 case "*":
-                    result = num1 * num2;
+                    temp = num1.multiply(num2);
+                    result = temp.doubleValue();
                     break;
                 case "/":
-                    result = num1 / num2;
+                    if (num2.compareTo(new BigDecimal(0)) == 0) {
+                        result = num1.doubleValue() / num2.doubleValue();
+                    }
+                    else {
+                        temp = num1.divide(num2, MathContext.DECIMAL64);
+                        result = temp.doubleValue();
+                    }
                     break;
             }
         }
-
         processResult();
     }
 
-    public void processResult(){
+    public void processResult() {
         output = result.toString();
         if (isInteger(result)) {
             Integer temp = result.intValue();
             txtScreen.setText(temp.toString());
         }
-        else
-            txtScreen.setText(result.toString());
+        else txtScreen.setText(result.toString());
     }
 
-    public boolean isInteger(Double a){
-        Integer b = a.intValue();
-        if (a - b == 0)
+    public boolean isInteger(Double a) {
+        if (a - a.intValue() == 0)
             return true;
-        else
-            return false;
+        return false;
     }
 
-    public void reset(){
+    public void reset() {
         isOperation = false;
         isFirstCalculation = true;
         isNumber = false;
@@ -288,6 +236,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         presentNum = "";
         txtScreen.setText("0");
     }
-
-
 }
